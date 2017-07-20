8874884faf642791a4743767249b4d367febc47c
Mahmoud Al-Qudsi <mqudsi@neosmart.net>
2017-07-20 17:23:04 -0500

    Added debug option

--- src/main.rs
+++ src/main.rs
@@ -19,18 +19,34 @@ fn main() {
              .takes_value(false)
              .short("i")
              .long("nocase")
-             .help("Disable case sensitivity"));
+             .help("Disable case sensitivity"))
+        .arg(clap::Arg::with_name("debug")
+             .takes_value(false)
+             .short("d")
+             .long("debug")
+             .help("Enable debug output"));
 
     let matches = app.get_matches();
     let globs = matches.values_of_lossy("glob").unwrap();
     let include_hidden = matches.is_present("all");
     let case_sensitive = !matches.is_present("nocase");
+    let debug = matches.is_present("debug");
 
     let options = glob::MatchOptions {
         case_sensitive: case_sensitive,
-        require_literal_separator: true,
         require_literal_leading_dot: !include_hidden,
+        require_literal_separator: true,
     };
+
+    if debug {
+        println!("");
+        println!("\toptions {{");
+        println!("\t\tcase_sensitive: {},", options.case_sensitive);
+        println!("\t\trequire_literal_leading_dot: {},", options.require_literal_leading_dot);
+        println!("\t\trequire_literal_separator: {}", options.require_literal_separator);
+        println!("\t}};");
+        println!("");
+    }
 
     for glob in globs {
         for entry in glob::glob_with(&glob, &options).unwrap() {
